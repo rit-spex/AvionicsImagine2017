cb807666a93cb18d4677038049ad8289faa15b3b
Austin Bodzas <abb6499@rit.edu>
2017-02-10 20:40:16 +0000

    basic rundown of imu sensor

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,30 @@
-void setup() {}
+#include <Wire.h>
 
-void loop() {}
+#include <SparkFunLSM9DS1.h>
+
+LSM9DS1 imu;
+void setup() {
+    init();
+    Serial.begin(9600);
+}
+
+void loop() {
+    imu.readAccel(); 
+    Serial.write("===============================")
+    Serial.write(String(imu.calcAccel(imu.ax), precision));
+    Serial.write(String(imu.calcAccel(imu.ay), precision));
+    Serial.write(String(imu.calcAccel(imu.az), precision));
+    Serial.write("===============================")
+    sleep(1); 
+}
+
+void init() {
+    //////////////////////////////////////////////////////////////////////////
+    /// Setup IMU
+    imu.settings.device.commInterface = IMU_MODE_I2C;
+    imu.settings.device.mAddress = LSM9DS1_M;
+    imu.settings.device.agAddress = LSM9DS1_AG;
+    
+    // TODO: error check and handle imu failing to init
+}
+
