12488bf74b37870a75e765bf617414a97758097a
unknown <ajh1450@rit.edu>
2017-02-15 19:04:22 -0500

    changed update time to 0.5s

--- web/pysockets.py
+++ web/pysockets.py
@@ -5,11 +5,13 @@ import random
 def main():
   HOST = 'localhost'
   PORT = 3000
+  DELAY = 0.5 #in seconds
+
   socketIO = SocketIO(HOST, PORT)
   while(True):
     xChange, yChange, zChange = readData()
     socketIO.emit('fromIMU', {'x':xChange, 'y':yChange, 'z':zChange})
-    time.sleep(0.2)
+    time.sleep(DELAY)
 def readData():
   x = random.uniform(-0.01,0.01);
   y = random.uniform(-0.01,0.01);
