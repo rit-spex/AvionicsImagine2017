1d1ee20b2dc8c02c85bc0df88aa49d1e30f9767b
Jeremy Quinn <jeremy.q97@gmail.com>
2017-03-04 13:18:05 -0500

    Added pitch/roll/yaw processing
    On computer side

--- web/serial_in.py
+++ web/serial_in.py
@@ -1,10 +1,12 @@
 import serial
 import ctypes
 import struct
+import math
 
 SOH = b'\x01'
 
 port = serial.Serial("/dev/ttyACM2")
+declination = 11.48
 
 def get_data():
     # wait on SOH character to read in all data values
@@ -13,6 +15,38 @@ def get_data():
             break
     return(struct.unpack('fffffffff', port.read(36)))
 
+def calculate_attitude():
+    data = get_data()
+    ax = data[0]
+    ay = data[1]
+    az = data[2]
+    mx = data[3]
+    my = data[4]
+    mz = data[5]
+
+    pitch = math.atan2(-ax, math.sqrt(ay * ay + az * az))
+    roll = math.atan2(ay, az)
+    heading
+
+    if(my == 0)
+        heading = (mx < 0) ? math.pi : 0
+    else
+        heading = math.atan2(mx, my)
+
+    heading -= declination * math.pi / 180.0
+
+    if(heading > math.pi)
+        heading -= 2 * math.pi
+    else if(heading < 0)
+        heading += 2 * math.pi
+
+    pitch *= 180.0 / math.pi
+    roll *= 180.0 / math.pi
+    heading *= 180.0 / math.pi
+
+    tup = (pitch, roll, heading)
+    return tup
+
 if __name__ == '__main__':
     while True:
-        print(get_data())
+        print(calculate_attitude())
